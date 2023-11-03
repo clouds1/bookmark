3611f2978af9898c9a5b9d5987e89f81df9efd83
clouds1 <digitalpaperbag@gmail.com>
2023-11-03 11:58:11 +1100

    Update getcerts.sh

--- getcerts.sh
+++ getcerts.sh
@@ -4,25 +4,27 @@
 LDAPS_SERVER="your-ldaps-server"
 LDAPS_PORT="636"
 
-# Name for the certificate file
-CERT_CHAIN="cert_chain.pem"
-
 # Connect to the LDAPS server and retrieve certificates
-openssl s_client -showcerts -connect $LDAPS_SERVER:$LDAPS_PORT </dev/null | awk '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/' > $CERT_CHAIN
+cert_chain=$(openssl s_client -showcerts -connect $LDAPS_SERVER:$LDAPS_PORT </dev/null)
 
-# Extract and import individual certificates
-CERT_COUNTER=1
-while true; do
-  CERT_FILE="cert$CERT_COUNTER.crt"
-  awk -v c=$CERT_COUNTER '/-----BEGIN CERTIFICATE-----/, /-----END CERTIFICATE-----/ {print $0 > "'$CERT_FILE'"} /-----END CERTIFICATE-----/ {c++;}' $CERT_CHAIN
-  if [ -s "$CERT_FILE" ]; then
-    keytool -import -noprompt -alias cert$CERT_COUNTER -file $CERT_FILE -keystore truststore.jks -storepass your-password
-    rm $CERT_FILE
-    CERT_COUNTER=$((CERT_COUNTER + 1))
-  else
-    break
+# Separate certificates and save them individually
+cert_counter=0
+while read -r cert; do
+  if [[ "$cert" == "-----END CERTIFICATE-----" ]]; then
+    cert_counter=$((cert_counter + 1))
+  fi
+  echo "$cert" >> "cert$cert_counter.crt"
+done <<< "$cert_chain"
+
+# Import individual certificates into the Java Truststore
+truststore="truststore.jks"
+password="your-password"
+
+for ((i = 0; i <= cert_counter; i++)); do
+  cert_file="cert$i.crt"
+  if [ -s "$cert_file" ]; then
+    keytool -import -noprompt -alias cert$i -file "$cert_file" -keystore "$truststore" -storepass "$password"
+    rm "$cert_file"
   fi
 done
 
-# Clean up the certificate chain file
-rm $CERT_CHAIN
