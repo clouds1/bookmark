74d99a30249efe05c46bd68d59eb50ef745e1d53
clouds1 <digitalpaperbag@gmail.com>
2023-11-03 12:08:38 +1100

    Update getcerts.sh

--- getcerts.sh
+++ getcerts.sh
@@ -7,24 +7,19 @@ LDAPS_PORT="636"
 # Connect to the LDAPS server and retrieve certificates
 cert_chain=$(openssl s_client -showcerts -connect $LDAPS_SERVER:$LDAPS_PORT </dev/null)
 
-# Separate certificates and save them individually
-cert_counter=0
-while read -r cert; do
-  if [[ "$cert" == "-----END CERTIFICATE-----" ]]; then
-    cert_counter=$((cert_counter + 1))
-  fi
-  echo "$cert" >> "cert$cert_counter.crt"
-done <<< "$cert_chain"
+# Use awk to separate certificates and save them individually
+awk -v RS="-----END CERTIFICATE-----" '/-----BEGIN CERTIFICATE-----/ {
+    print $0 "-----END CERTIFICATE-----" > ("cert" ++cert_counter ".crt")
+}' <<< "$cert_chain"
 
 # Import individual certificates into the Java Truststore
 truststore="truststore.jks"
 password="your-password"
 
-for ((i = 0; i <= cert_counter; i++)); do
+for ((i = 1; i <= cert_counter; i++)); do
   cert_file="cert$i.crt"
   if [ -s "$cert_file" ]; then
     keytool -import -noprompt -alias cert$i -file "$cert_file" -keystore "$truststore" -storepass "$password"
     rm "$cert_file"
   fi
 done
-
